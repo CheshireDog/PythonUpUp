61d3e0343672797126bcb987dd7e169069a9c4a0
hejunhao <0049003071@znv.com>
2020-05-25 12:35:21 +0800

    four number sum solved2

--- Algorithm/4sum.py
+++ Algorithm/4sum.py
@@ -40,13 +40,13 @@ class Solution(object):
 
     def fourSum(self, number_list, target):
         length = len(number_list)
-        nums = sorted(number_list)
-        # nums = number_list
+        # nums = sorted(number_list)
+        nums = number_list
         dict = {}
         result = []
         if length < 4:
             return []
-        if length == 4 :
+        if length == 4:
             if sum(nums) == target:
                 return [nums]
             else:
@@ -59,7 +59,7 @@ class Solution(object):
                 else:
                     key = nums[i]+nums[j]
                     dict[key] = [(i, j)]
-        print(dict)
+        # print(dict)
 
         for key in dict:
             if (target-key) in dict.keys():
@@ -67,31 +67,33 @@ class Solution(object):
                     if len(dict[key]) > 1:
                         for p in range(len(dict[key])-1):
                             for q in range(p+1, len(dict[key])):
-                                ind1 = dict[key][p][0]
-                                ind2 = dict[key][p][1]
-                                ind3 = dict[key][q][0]
-                                result.append((nums[dict[key][p][0]], nums[dict[key][p][1]], nums[dict[key][q][0]],
-                                               nums[dict[key][q][1]]))
+                                ind = [dict[key][p][0],dict[key][p][1],dict[key][q][0],dict[key][q][1]]
+                                # print(ind)
+                                if len(set(ind)) == 4:
+                                    # sorted(ind)
+                                    ind.sort()
+                                    # print(ind)
+                                    # print((nums[ind[0]], nums[ind[1]], nums[ind[2]],nums[ind[3]]))
+                                    result.append((nums[ind[0]], nums[ind[1]], nums[ind[2]],
+                                                   nums[ind[3]]))
                 else:
                     for index in dict[key]:
                         for index2 in dict[target-key]:
-                            result.append((nums[index[0]], nums[index[1]], nums[index2[0]], nums[index2[1]],))
-
-        # for p in range(2, length-1):
-        #     for q in range(p+1, length):
-        #         sub = target-nums[p]-nums[q]
-        #         if sub in dict:
-        #             for index in dict[sub]:
-        #                 if index[1] < p:
-        #                     result.append((nums[index[0]], nums[index[1]], nums[p], nums[q]))
+                            if len({index[0], index[1], index2[0], index2[1]}) == 4:
+                                ind = sorted([index[0], index[1], index2[0], index2[1]])
+                                # print(ind)
+                                result.append((nums[ind[0]], nums[ind[1]], nums[ind[2]], nums[ind[3]],))
         return list(set(result))
 
 if __name__ == '__main__':
-    num_list = [-5,5,4,-3,0,0,4,-2]
-    target = 4
+    num_list = [1,0,-1,0,-2,2]
+    print(num_list)
+    target = 0
     so = Solution()
     print(so.four_sum(num_list, target))
     print(so.fourSum(num_list, target))
+
+
 
 
 
